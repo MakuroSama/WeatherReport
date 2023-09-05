06975c829b1415464abdf9d0c57e19cd93138cf8
pumpprach <131134025+pumpprach@users.noreply.github.com>
2023-09-05 15:01:24 +0700

    Add files via upload

--- sketch_sep5a/sketch_sep5a.ino
+++ sketch_sep5a/sketch_sep5a.ino
@@ -1,106 +1,78 @@
-/*
-  Rui Santos
-  Complete project details at our blog.
-    - ESP32: https://RandomNerdTutorials.com/esp32-firebase-realtime-database/
-    - ESP8266: https://RandomNerdTutorials.com/esp8266-nodemcu-firebase-realtime-database/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-  Based in the RTDB Basic Example by Firebase-ESP-Client library by mobizt
-  https://github.com/mobizt/Firebase-ESP-Client/blob/main/examples/RTDB/Basic/Basic.ino
-*/
-
 #include <Arduino.h>
-#if defined(ESP32)
-  #include <WiFi.h>
-#elif defined(ESP8266)
-  #include <ESP8266WiFi.h>
-#endif
-#include <Firebase_ESP_Client.h>
+#include <ESP8266WiFi.h>
+#include <Hash.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
-//Provide the token generation process info.
-#include "addons/TokenHelper.h"
-//Provide the RTDB payload printing info and other helper functions.
-#include "addons/RTDBHelper.h"
 
-// Insert your network credentials
-#define WIFI_SSID "TT"
-#define WIFI_PASSWORD ""
 
-// Insert Firebase project API Key
-#define API_KEY "AIzaSyA5UQLbdakG7d0u69E6V2L6e_0oKuN9LLY"
+#define DHTPIN 4   // Digital pin connected to the DHT sensor
 
-// Insert RTDB URLefine the RTDB URL */
-#define DATABASE_URL "https://realtimeweatherreport-default-rtdb.asia-southeast1.firebasedatabase.app/" 
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11    
 
-//Define Firebase Data object
-FirebaseData fbdo;
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
-FirebaseAuth auth;
-FirebaseConfig config;
+DHT dht(DHTPIN, DHTTYPE);
 
-unsigned long sendDataPrevMillis = 0;
-int count = 0;
-bool signupOK = false;
+// current temperature & humidity, updated in loop()
+float t = 0.0;
+float h = 0.0;
+
+// Create AsyncWebServer object on port 80
+
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;    // will store last time DHT was updated
+
+// Updates DHT readings every 10 seconds
+const long interval = 10000;  
+
+
 
 void setup(){
+  // Serial port for debugging purposes
   Serial.begin(115200);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(300);
-  }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-
-  /* Assign the api key (required) */
-  config.api_key = API_KEY;
-
-  /* Assign the RTDB URL (required) */
-  config.database_url = DATABASE_URL;
-
-  /* Sign up */
-  if (Firebase.signUp(&config, &auth, "", "")){
-    Serial.println("ok");
-    signupOK = true;
-  }
-  else{
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
-  }
-
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  dht.begin();
   
-  Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
-}
+  // Connect to Wi-Fi
+ 
+  }
 
-void loop(){
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
-    sendDataPrevMillis = millis();
-    // Write an Int number on the database path test/int
-    if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-    }
-    else {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
-    count++;
+
+  
+ 
+void loop(){  
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you updated the DHT values
+    previousMillis = currentMillis;
+    // Read temperature as Celsius (the default)
+    float newT = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //float newT = dht.readTemperature(true);
+    // if temperature read failed, don't change t value
     
-    // Write an Float number on the database path test/float
-    if (Firebase.RTDB.setFloat(&fbdo, "test/float", 0.01 + random(0,100))){
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
+    if (isnan(newT)) {
+      Serial.println("Failed to read from DHT sensor!");
     }
     else {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
+      t = newT;
+      Serial.println(t);
+    }
+    // Read Humidity
+    float newH = dht.readHumidity();
+    // if humidity read failed, don't change h value 
+    if (isnan(newH)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      h = newH;
+      Serial.println(h);
+
     }
   }
-}
+
+  }
+   
